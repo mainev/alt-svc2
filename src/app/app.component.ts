1af671147c4f09bfd06208fb15387bbdaafbe60f
mainevillarias <mainevillarias@gmail.com>
2018-02-13 00:17:54 +0800

    initialization

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  exports: [MatButtonModule, MatCheckboxModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
